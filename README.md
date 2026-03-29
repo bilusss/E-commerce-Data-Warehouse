abd1a0808663b15272400f9aa7d67ce6b2a8432c
Łukasz Bilski <bukaszlilski@gmail.com>
2026-03-29 15:07:25 +0200

    transform done

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # E-commerce-Data-Warehouse
+
+Using OLAP structure for better performance for data analysis
+
+
+**`fact_orders`** 
+| order_id | customer_id | order_status | order_purchase_timestamp | product_id | seller_id | price | freight_value | revenue | total_payment | payment_installments | payment_type | delivery_days | delay_days |
+|---|---|---|---|---|---|---:|---:|---:|---:|---:|---|---:|---:|
+| e481f51cbdc54678b7cc49136f2d6af7 | 9ef432eb6251297304e76186b10a928d | delivered | 2017-10-02 10:56:33 | 87285b34884572647811a353c7ac498a | 3504c0cb71d7fa48d967e0e4c94d59d9 | 29.99 | 8.72 | 38.71 | 38.71 | 1 | credit_card | 8 | -8 |
+| 53cdb2fc8bc7dce0b6741e2150273451 | b0830fb4747a6c6d20dea0b8c802d7ef | delivered | 2018-07-24 20:41:37 | 595fac2a385ac33a80bd5114aec74eb8 | 289cdb325fb7e7f891c38608bf9e0962 | 118.70 | 22.76 | 141.46 | 141.46 | 1 | boleto | 13 | -6 |
+
+
+**`dim_customer`**
+| customer_id | customer_unique_id | customer_city | customer_state | customer_zip_code_prefix | geolocation_lat | geolocation_lng |
+|---|---|---|---|---:|---:|---:|
+| 06b8999e2fba1a1fbc88172c00ba8bc7 | 861eff4711a542e4b93843c6dd7febb0 | franca | SP | 14409 | -20.498489 | -47.396929 |
+| 18955e83d337fd6b2def6b18a428ac77 | 290c77bc529b7ac935b93aa66c333dc3 | sao bernardo do campo | SP | 9790 | -23.727992 | -46.542848 |
+
+**`dim_product`**
+| product_id | category_en | product_weight_g | product_length_cm | product_height_cm | product_width_cm | product_photos_qty |
+|---|---|---:|---:|---:|---:|---:|
+| 1e9e8ef04dbcff4541ed26657ea517e5 | perfumery | 225 | 16 | 10 | 14 | 1 |
+| 3aa071139cb16b67ca9e5dea641aaa2f | art | 1000 | 30 | 18 | 20 | 1 |
+
+**`dim_seller`**
+| seller_id | seller_city | seller_state | seller_zip_code_prefix | geolocation_lat | geolocation_lng |
+|---|---|---|---:|---:|---:|
+| 3442f8959a84dea7ee197c632cb2df15 | campinas | SP | 13023 | -22.893848 | -47.061337 |
+| d1b65fc7debc3361ea86b5f14c68d2e2 | mogi guacu | SP | 13844 | -22.383437 | -46.947927 |
+
+**`dim_date`**
+| date | purchase_hour | purchase_dow | day_part | year | month | day |
+|---|---:|---|---|---:|---:|---:|
+| 2017-10-02 10:56:33 | 10 | Monday | morning | 2017 | 10 | 2 |
+| 2018-07-24 20:41:37 | 20 | Tuesday | evening | 2018 | 7 | 24 |
+
+**`dim_review`**
+| review_id | order_id | review_score | sentiment | response_time_days |
+|---|---|---:|---|---:|
+| 7bc2406110b926393aa56f80a40eba40 | 73fc7af87114b39712e6da79b0a377eb | 4 | positive | 0 |
+| 80e641a11e56f04c1ad469d5645fdfde | a548910a1c6147796b98fdf73dbeba33 | 5 | positive | 1 |
+
+---
--- src/transform.py
+++ src/transform.py
@@ -14,56 +14,133 @@ def transform(dfs: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
   sellers         = dfs["sellers"].copy()
   category_names  = dfs["category_names"].copy()
 
-  # print("=== CUSTOMERS ===")
-  customers["customer_zip_code_prefix"] = customers["customer_zip_code_prefix"].astype(pd.Int64Dtype())
+  """
+  Data types handling
+  """
+  logger.info("Started changing data types for pandas-like")
 
-  # NaN handling
-  # print(customers.isnull().mean() * 100)
-  # print(customers["order_status"].value_counts())
-  print(customers.info())
-  with pd.option_context('display.max_columns', None):
-    print(customers.sample(n=5))
+  logger.debug("Changing data types - customers")
+  customers["customer_zip_code_prefix"] = customers["customer_zip_code_prefix"].astype(pd.Int64Dtype())
   
-  # print("\n=== GEOLOCATION ===")
+  logger.debug("Changing data types - geolocation")
   geolocation["geolocation_zip_code_prefix"] = geolocation["geolocation_zip_code_prefix"].astype(pd.Int32Dtype())
   geolocation["geolocation_lat"] = geolocation["geolocation_lat"].astype(pd.Float64Dtype())
   geolocation["geolocation_lng"] = geolocation["geolocation_lng"].astype(pd.Float64Dtype())
 
-  # print("\n=== ORDER_ITEMS ===")
+  logger.debug("Changing data types - order_items")
   order_items["order_item_id"] = order_items["order_item_id"].astype(pd.Int64Dtype())
   order_items["shipping_limit_date"] = pd.to_datetime(order_items["shipping_limit_date"], errors="coerce")
   order_items["price"] = order_items["price"].astype(pd.Float64Dtype())
   order_items["freight_value"] = order_items["freight_value"].astype(pd.Float64Dtype())
 
-  # print("\n=== PAYMENTS ===")
+  logger.debug("Changing data types - payments")
   payments["payment_sequential"] = payments["payment_sequential"].astype(pd.Int8Dtype())
-  payments["payment_installments"] = payments["payment_installments"].astype(pd.Int64Dtype())
+  payments["payment_installments"] = payments["payment_installments"].astype(pd.Int16Dtype())
   payments["payment_value"] = payments["payment_value"].astype(pd.Float64Dtype())
 
-  # print("\n=== REVIEWS ===")
+  logger.debug("Changing data types - reviews")
   reviews["review_score"] = reviews["review_score"].astype(pd.Int8Dtype())
   reviews["review_creation_date"] = pd.to_datetime(reviews["review_creation_date"], errors="coerce")
   reviews["review_answer_timestamp"] = pd.to_datetime(reviews["review_answer_timestamp"], errors="coerce")
 
-  # print("\n=== ORDERS ===")
+  logger.debug("Changing data types - orders")
   date_cols = ["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date",
                         "order_delivered_customer_date", "order_estimated_delivery_date"]
   for col in date_cols:
     orders[col] = pd.to_datetime(orders[col], errors="coerce")
 
-  # print("\n=== PRODUCTS ===")
+  logger.debug("Changing data types - products")
+  products["product_photos_qty"] = products["product_photos_qty"].astype(pd.Int16Dtype())
   float_cols = ["product_name_lenght",
-       "product_description_lenght", "product_photos_qty", "product_weight_g",
+       "product_description_lenght", "product_weight_g",
        "product_length_cm", "product_height_cm", "product_width_cm"]
   
   for col in float_cols:
     products[col] = products[col].astype(pd.Float64Dtype())
 
-  # print("\n=== SELLERS ===")
+  logger.debug("Changing data types - sellers")
   sellers["seller_zip_code_prefix"] = sellers["seller_zip_code_prefix"].astype(pd.Int64Dtype())
 
-  # print("\n=== CATEGORY_NAMES ===")
-  
+  """
+  Feature engieneering
+  """
+  logger.info(f"Started feature engineering")
 
+  logger.debug("Feature engineering - reviews")
+  reviews["response_time_days"] = (reviews["review_answer_timestamp"] - reviews["review_creation_date"]).dt.days
+  reviews["sentiment"] = pd.cut(reviews["review_score"], bins=[0, 2, 3, 5], labels=["negative", "neutral", "positive"])
+
+  logger.debug("Feature engineering - orders")
+  orders["delivery_days"] = (orders["order_delivered_customer_date"] - orders["order_purchase_timestamp"]).dt.days
+  orders["delay_days"] = (orders["order_delivered_customer_date"] - orders["order_estimated_delivery_date"]).dt.days
+  orders["purchase_hour"] = orders["order_purchase_timestamp"].dt.hour
+  orders["purchase_dow"]  = orders["order_purchase_timestamp"].dt.day_name()
+  orders["day_part"] = pd.cut(orders["purchase_hour"], bins=[0, 6, 12, 18, 24],
+                          labels=["night", "morning", "afternoon", "evening"],right=False)
   
-  return dfs
+  logger.debug("Feature engineering - order_items")
+  order_items["revenue"] = order_items["price"] + order_items["freight_value"]
+  # freight value = transport cost
+
+
+  logger.debug(f"Started aggregation")
+  payment_agg = payments.groupby("order_id").agg(total_payment=("payment_value", "sum"),
+                          payment_installments=("payment_installments", "max"), payment_type=("payment_type", "first")).reset_index()
+
+  geo_agg = (geolocation.groupby("geolocation_zip_code_prefix")[["geolocation_lat", "geolocation_lng"]].mean().reset_index())
+
+
+  logger.info(f"Started creating star schema")
+  logger.debug(f"Started creating fact table")
+  fact_orders = (
+    orders[["order_id", "customer_id", "order_status", "order_purchase_timestamp", "delivery_days", "delay_days",
+            "purchase_hour", "purchase_dow", "day_part"]]
+    .merge(
+        order_items[["order_id", "product_id", "seller_id", "price", "freight_value", "revenue"]], on="order_id", how="left"
+    )
+    .merge(payment_agg, on="order_id", how="left")
+  )
+
+  logger.debug(f"Started creating dimensional tables")
+  dim_customer = (
+    customers[["customer_id", "customer_unique_id", "customer_city", "customer_state", "customer_zip_code_prefix"]]
+    .drop_duplicates()
+    .merge(geo_agg, left_on="customer_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="left")
+    .drop(columns="geolocation_zip_code_prefix")
+  )
+
+  dim_seller = (
+    sellers[["seller_id", "seller_city", "seller_state", "seller_zip_code_prefix"]]
+    .drop_duplicates()
+    .merge(geo_agg, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="left")
+    .drop(columns="geolocation_zip_code_prefix")
+  )
+
+  dim_product = (
+    products.merge(category_names, on="product_category_name", how="left")
+    .assign(category_en=lambda x: x["product_category_name_english"].fillna("unknown"))
+  )[["product_id", "category_en", "product_weight_g", "product_length_cm", "product_height_cm", "product_width_cm",
+    "product_photos_qty"]].drop_duplicates()
+
+  dim_date = (
+    orders[["order_purchase_timestamp", "purchase_hour", "purchase_dow", "day_part"]]
+    .drop_duplicates(subset=["order_purchase_timestamp"])
+    .assign(
+        year=lambda x: x["order_purchase_timestamp"].dt.year,
+        month=lambda x: x["order_purchase_timestamp"].dt.month,
+        day=lambda x: x["order_purchase_timestamp"].dt.day,
+    )
+    .rename(columns={"order_purchase_timestamp": "date"})
+  )
+
+  dim_review = reviews[["review_id", "order_id", "review_score", "sentiment", "response_time_days"]].drop_duplicates()
+
+  logger.info(f"Completed doing 1 fact table and 5 dimension tables")
+  return {
+    "fact_orders":   fact_orders,
+    "dim_customer":  dim_customer,
+    "dim_product":   dim_product,
+    "dim_seller":    dim_seller,
+    "dim_date":      dim_date,
+    "dim_review":    dim_review,
+  }
